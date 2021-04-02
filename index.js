8cd4a2b900dc2c042d588fe6ed14f305bea84bb2
DavidRMorphew <morphewd@umich.edu>
2021-04-02 19:30:43 -0400

    commit tree 84275f33cfc85547fd195bd5740a744cb379e406

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ function findMatching(driversArray, name){
     return driversArray.filter( element => element.toLowerCase() === name.toLowerCase() )
 }
 
+// function fuzzyMatch(driversArray, firstPartOfName){
+//     return driversArray.filter( element => {
+//         const truncatedStringLength = firstPartOfName.length
+//         const truncatedName = element.slice(0, truncatedStringLength)
+//         return truncatedName === firstPartOfName
+//     })
+// }
+
 function fuzzyMatch(driversArray, firstPartOfName){
-    return driversArray.filter( element => {
-        const truncatedStringLength = firstPartOfName.length
-        const truncatedName = element.slice(0, truncatedStringLength)
-        return truncatedName === firstPartOfName
-    })
+    return driversArray.filter( element => element.slice(0, firstPartOfName.length) === firstPartOfName )
 }
 
 function matchName(driversArray, nameString){
