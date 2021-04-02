85848c6b6ca7b7a4256b51dd67760be264cf79bc
DavidRMorphew <morphewd@umich.edu>
2021-04-02 18:01:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(driversArray, name){
+    return driversArray.filter( element => {
+        return element.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(driversArray, firstPartOfName){
+    return driversArray.filter( element => {
+        const truncatedStringLength = firstPartOfName.length
+        const truncatedName = element.slice(0, truncatedStringLength - 1)
+        console.log(truncatedName)
+    })
+}
